9a13f671009e9577c2ab44bd9c19c739f0ef74fb
yann <yann.faussette@mytulip.io>
2023-06-17 21:23:54 +0200

    Switch to EsModule Add logs Fix waiting bugs Add test solution watcher

--- src/index.js
+++ src/index.js
@@ -1,77 +1,93 @@
-const puppeteer = require('puppeteer');
-const {Configuration, OpenAIApi} = require('openai');
-require('dotenv').config();
+import puppeteer from 'puppeteer';
+import { Configuration, OpenAIApi } from 'openai';
+import { config as dotenvConfig } from 'dotenv';
+import chalk from 'chalk';
 
-const openAPIConf = new Configuration({apiKey: process.env.OPENAI_API_KEY});
+dotenvConfig();
+
+const openAPIConf = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
 const openai = new OpenAIApi(openAPIConf);
 
 async function solveProblem() {
-    // Ouvrez un nouveau navigateur et allez à Codewars
-    const browser = await puppeteer.launch({headless: false});
+    console.log(chalk.blue('Starting the browser...'));
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
+
+    console.log(chalk.blue('Navigating to Codewars...'));
     await page.goto('https://www.codewars.com/users/sign_in');
 
-    // Connectez-vous à Codewars
+    console.log(chalk.blue('Logging in...'));
     await page.type('#user_email', process.env.CODINGWAR_EMAIL);
     await page.type('#user_password', process.env.CODINGWAR_PASSWORD);
-    // click on button write Sign in
     const [button] = await page.$x("//button[contains(text(), 'Sign in')]");
     if (button) {
         await button.click();
     } else {
-        throw new Error("Button not found");
+        throw new Error("Sign in button not found");
     }
 
-    // Accédez à un problème
+    console.log(chalk.blue('Navigating to problem...'));
     await page.waitForNavigation();
     await page.goto('https://www.codewars.com/dashboard');
-    await page.click('#personal-trainer-play')
+    await page.click('#personal-trainer-play');
 
-    // Attendez que le problème se charge
-    await page.waitForNavigation();
-
-    await page.waitForTimeout(6000); // Attendre 6 secondes
+    console.log(chalk.blue('Waiting for the problem to load...'));
+    await page.waitForSelector('.CodeMirror', { visible: true });
 
 
-// Attendez que l'élément avec l'id `description` soit rendu
-    await page.waitForSelector('#description');
-
-
-// Obtenez les détails du problème
+    console.log(chalk.blue('Fetching problem details...'));
     const problemDetails = await page.evaluate(() => {
         const descriptionElement = document.querySelector('#description');
-
-        if (descriptionElement) {
-            return descriptionElement.innerText;
-        } else {
-            throw new Error('Element with id "description" not found');
-        }
+        return descriptionElement ? descriptionElement.innerText : null;
     });
 
-    console.log(problemDetails)
-    // Utilisez GPT-4 pour générer une solution
-    const prompt = `Solve this coding problem in javascript. Please return only the raw code without any Markdown formatting: ${problemDetails}`;
+    console.log(chalk.blue('Generating solution...'));
+    const prompt = `Solve this coding problem in JavaScript: ${problemDetails}`;
     const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
-        messages: [
-            {role: "user", content: prompt},
-        ]
-    })
-    console.log(chatCompletion.data.choices[0].message)
-// Entrez la solution dans l'éditeur de code
+        messages: [{role: "user", content: prompt}]
+    });
+    const solution = chatCompletion.data.choices[0].message.content;
+
+    console.log(chalk.blue('Entering solution in code editor...'));
     await page.evaluate((solution) => {
-        // Trouvez l'instance de l'éditeur CodeMirror sur la page
-        // Adaptez ceci en fonction de la structure exacte de la page
         const codeMirror = document.querySelector('.CodeMirror').CodeMirror;
         codeMirror.setValue(solution);
-    }, chatCompletion.data.choices[0].message.content);
+    }, solution);
 
-    // Tester la solution
-   await page.click('#validate_btn');
+    console.log(chalk.blue('Testing the solution...'));
+    await page.click('#validate_btn');
+    await page.waitForTimeout(10000);
 
+    console.log(chalk.blue('Checking test results...'));
+    // Get the iframe element handle
+    const frameElement = await page.$('#runner_frame');
+    // Ensure the iframe was found
+    if (!frameElement) {
+        throw new Error('Runner iframe not found');
+    }
+    // Get the frame object from the element handle
+    const resultFrame = await frameElement.contentFrame();
+    // Now, you can interact with the iframe as if it's a page
+    const testsPassed = await resultFrame.evaluate(() => {
+        const messageElement = document.querySelector('.run-results__congrats');
+        return messageElement && window.getComputedStyle(messageElement).display !== 'none' && messageElement.innerText.includes("You have passed all of the tests! :)");
+    });
 
-    // Fermez le navigateur
-    //await browser.close();
+    if (testsPassed) {
+        console.log(chalk.green('All tests passed. You are so smart!'));
+
+    } else {
+        console.log(chalk.red('Not all tests passed.'));
+    }
+
+    console.log(chalk.blue('Closing the browser...'));
+    await browser.close();
 }
 
-solveProblem().catch(console.error);
+solveProblem()
+    .then(() => process.exit(0))
+    .catch(error => {
+        console.error(chalk.red(error))
+        process.exit(1)
+    });
