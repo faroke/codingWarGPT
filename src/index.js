ab947383ed8ae452f10cedba54b722c48757aa63
yann <yann.faussette@mytulip.io>
2023-06-17 22:15:07 +0200

    Removing useless logs Adding SQLITE DB Adding clusters

--- src/index.js
+++ src/index.js
@@ -1,22 +1,39 @@
 import puppeteer from 'puppeteer';
-import { Configuration, OpenAIApi } from 'openai';
-import { config as dotenvConfig } from 'dotenv';
+import {Configuration, OpenAIApi} from 'openai';
+import {config as dotenvConfig} from 'dotenv';
 import chalk from 'chalk';
+import os from 'os';
+import cluster from 'cluster';
+import Database from 'better-sqlite3';
+
+
 
 dotenvConfig();
 
-const openAPIConf = new Configuration({ apiKey: process.env.OPENAI_API_KEY });
+let solutionsCount = 0;
+let failuresCount = 0;
+
+
+const openAPIConf = new Configuration({apiKey: process.env.OPENAI_API_KEY});
 const openai = new OpenAIApi(openAPIConf);
 
+const db = new Database('solutions.db');
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS solutions (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        url TEXT,
+        solution TEXT,
+        name TEXT
+    );
+`);
+
 async function solveProblem() {
-    console.log(chalk.blue('Starting the browser...'));
-    const browser = await puppeteer.launch({ headless: false });
+    const browser = await puppeteer.launch({headless: 'new'});
     const page = await browser.newPage();
 
-    console.log(chalk.blue('Navigating to Codewars...'));
     await page.goto('https://www.codewars.com/users/sign_in');
 
-    console.log(chalk.blue('Logging in...'));
     await page.type('#user_email', process.env.CODINGWAR_EMAIL);
     await page.type('#user_password', process.env.CODINGWAR_PASSWORD);
     const [button] = await page.$x("//button[contains(text(), 'Sign in')]");
@@ -26,22 +43,21 @@ async function solveProblem() {
         throw new Error("Sign in button not found");
     }
 
-    console.log(chalk.blue('Navigating to problem...'));
     await page.waitForNavigation();
     await page.goto('https://www.codewars.com/dashboard');
     await page.click('#personal-trainer-play');
 
-    console.log(chalk.blue('Waiting for the problem to load...'));
-    await page.waitForSelector('.CodeMirror', { visible: true });
+    await page.waitForSelector('.CodeMirror', {visible: true});
+
+    const fullTitle = await page.title();
+    const name = fullTitle.split('|')[0].trim(); // Get the problem's name by splitting the title by '|' and taking the first part
 
 
-    console.log(chalk.blue('Fetching problem details...'));
     const problemDetails = await page.evaluate(() => {
         const descriptionElement = document.querySelector('#description');
         return descriptionElement ? descriptionElement.innerText : null;
     });
 
-    console.log(chalk.blue('Generating solution...'));
     const prompt = `Solve this coding problem in JavaScript: ${problemDetails}`;
     const chatCompletion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
@@ -49,17 +65,14 @@ async function solveProblem() {
     });
     const solution = chatCompletion.data.choices[0].message.content;
 
-    console.log(chalk.blue('Entering solution in code editor...'));
     await page.evaluate((solution) => {
         const codeMirror = document.querySelector('.CodeMirror').CodeMirror;
         codeMirror.setValue(solution);
     }, solution);
 
-    console.log(chalk.blue('Testing the solution...'));
     await page.click('#validate_btn');
     await page.waitForTimeout(10000);
 
-    console.log(chalk.blue('Checking test results...'));
     // Get the iframe element handle
     const frameElement = await page.$('#runner_frame');
     // Ensure the iframe was found
@@ -75,19 +88,56 @@ async function solveProblem() {
     });
 
     if (testsPassed) {
-        console.log(chalk.green('All tests passed. You are so smart!'));
+        process.send({ solutionFound: true });
+
+        const code = await page.evaluate(() => {
+            const codeMirror = document.querySelector('.CodeMirror').CodeMirror;
+            return codeMirror.getValue();
+        });
+
+
+
+        // Insert into the SQLite database
+        const stmt = db.prepare("INSERT INTO solutions (url, solution, name) VALUES (?, ?, ?)");
+        stmt.run(page.url(), solution, name);
 
     } else {
-        console.log(chalk.red('Not all tests passed.'));
+        process.send({ solutionFound: false });
     }
 
-    console.log(chalk.blue('Closing the browser...'));
     await browser.close();
 }
 
-solveProblem()
-    .then(() => process.exit(0))
-    .catch(error => {
-        console.error(chalk.red(error))
-        process.exit(1)
+if (cluster.isMaster) {
+    console.log(`Master ${process.pid} is running`);
+    const numWorkers = 2 /*os.cpus().length*/;
+    // Fork workers.
+    for (let i = 0; i < numWorkers; i++) {
+        cluster.fork();
+    }
+
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died. Restarting...`);
+        cluster.fork();
     });
+
+    cluster.on('message', (worker, message) => {
+        if (message.solutionFound) {
+            solutionsCount++;
+            console.log(chalk.green(`Solutions found: ${solutionsCount}`));
+        }
+        else {
+            failuresCount++;
+            console.log(chalk.red(`Failures: ${failuresCount}`));
+        }
+    });
+
+} else {
+    console.log(`Worker ${process.pid} started`);
+    solveProblem()
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(chalk.red(error))
+            process.exit(1)
+        });
+}
